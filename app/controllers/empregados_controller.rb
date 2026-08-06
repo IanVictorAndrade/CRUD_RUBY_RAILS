d39ef1015e693db6f442a44c1e2e2b9237c1b2de
ian victor barbosa de andrade <ian.andrade@sefin.ro.gov.br>
2026-08-06 12:50:32 -0400

    feat: listando funcionário numa view

--- app/controllers/empregados_controller.rb
+++ app/controllers/empregados_controller.rb
@@ -1,2 +1,13 @@
 class EmpregadosController < ApplicationController
+  def index
+    @empregados = Empregado.all
+  end
+
+  def show
+    @empregado = Empregado.find(params[:id])
+  end
+
+  def edit
+    @empregado = Empregado.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  # resources :empregados, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "empregados" => "empregados#index", as: "empregados"
+
+  get "show/:id" => "empregados#show", as: "exibicao"
 end
