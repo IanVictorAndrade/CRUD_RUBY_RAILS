b33a04cb785378a75a13315f3957e91b93ca2f5f
ian victor barbosa de andrade <ian.andrade@sefin.ro.gov.br>
2026-08-28 08:45:08 -0400

    feat: cookies state management

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,15 +1,13 @@
 class EmployeesController < ApplicationController
 
-
   def index
-
     # Plain Text, Signed, Encrypted
 
     cookies[:emp_name] = "Hey! I am a Plain Text Cookie"
 
     cookies[:exp_cookies] = { value: "Anthony Joshua", expires: Time.now + 2.minutes }
 
-    cookies.signed[:user_id] = "Hey! I can be decoded, but cant be modified as I am cryptographically signed!"
+    cookies.signed[:user_id] = "Hey! i can be decoded, but cant be modified as I am cryptographically signed"
 
     cookies.encrypted[:credit_card] = 'Hey! I cant be decoded nor can be modified as I am authenticated at the time of decryption'
 
@@ -19,16 +17,4 @@ class EmployeesController < ApplicationController
 
   end
 
-  def sessiondemo
-    session[:emp_id] = 123
-    session[:salary] = 16775
-    session[:is_active] = true
-    session[:emp_name] = 'Chad Mendis'
-  end
-
-  def readsessiondata
-    session.delete(:emp_id)
-    session.clear
-    reset_session
-  end
 end
