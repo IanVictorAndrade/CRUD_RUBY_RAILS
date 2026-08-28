8e7dc2895669bb34f9bf1b43c3009a52905afe69
ian victor barbosa de andrade <ian.andrade@sefin.ro.gov.br>
2026-08-28 07:46:57 -0400

    feat: session demo with persistence on database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_27_145947) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_27_170405) do
   create_table "accounts", force: :cascade do |t|
     t.decimal "balance"
     t.datetime "created_at", null: false
@@ -174,6 +174,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_27_145947) do
     t.datetime "created_at", null: false
     t.string "monthname"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "data"
+    t.string "session_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "states", force: :cascade do |t|
